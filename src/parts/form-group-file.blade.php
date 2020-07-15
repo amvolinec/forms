486a35fd234430b0438e26610f629b94afa71f73
medumi <medumi@icloud.com>
2020-07-15 06:20:13 +0300

    changed base route to admin-panel

---
+++ src/parts/form-group-file.blade.php
@@ -0,0 +1,5 @@
+
+                            <div class="form-group">
+                                <label for="fileUpload">{{ __('%2$s') }}</label>
+                                <input type="file" class="form-control-file" id="fileUpload" name="%1$s" value="{{ $%4$s->%1$s ?? old('%1$s') }}" %3$s>
+                            </div>
