ab4ed938b6432883f7f04e7d954c519db6fe5773
medumi <medumi@icloud.com>
2020-08-04 18:54:04 +0300

    forms/ changed to av-panel/

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,12 @@
 <?php
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get('forms/', 'Avart\Forms\FieldsController@index')->name('forms.index');
+    Route::get('av-panel/', 'Avart\Forms\FieldsController@index')->name('forms.index');
     Route::get('get-index/{table}', 'Avart\Forms\FieldsController@getIndex')->name('forms.get_index');
-    Route::get('forms/create', 'Avart\Forms\FieldsController@create')->name('forms.create');
-    Route::post('forms/store', 'Avart\Forms\FieldsController@store')->name('forms.store');
-    Route::get('forms/{route}/edit', 'Avart\Forms\FieldsController@edit')->name('forms.edit');
-    Route::put('forms/{route}/update', 'Avart\Forms\FieldsController@update')->name('forms.update');
+    Route::get('av-panel/create', 'Avart\Forms\FieldsController@create')->name('forms.create');
+    Route::post('av-panel/store', 'Avart\Forms\FieldsController@store')->name('forms.store');
+    Route::get('av-panel/{route}/edit', 'Avart\Forms\FieldsController@edit')->name('forms.edit');
+    Route::put('av-panel/{route}/update', 'Avart\Forms\FieldsController@update')->name('forms.update');
 });
 
 
