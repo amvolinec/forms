9df2cbea1af3f1ab955cf576067d61b69c130725
medumi <medumi@icloud.com>
2020-09-03 21:35:09 +0300

    File upload

--- src/parts/form-group-file.blade.php
+++ src/parts/form-group-file.blade.php
@@ -1,8 +1,11 @@
-                           <div class="input-group">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input"
-                                           id="fileUpload" value="{{ $%4$s->%1$s ?? old('%1$s') }}"
-                                           aria-describedby="inputGroupFileAddon01" name="%1$s" %3$s>
-                                    <label class="custom-file-label" for="fileUpload">{{ __('%2$s') }}</label>
+                            <div class="form-group row">
+                                <div class="col-md-4"></div>
+                                <div class="input-group col-md-6">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input"
+                                               id="fileUpload" value="{{ $%4$s->%1$s ?? old('%1$s') }}"
+                                               aria-describedby="inputGroupFileAddon01" name="%1$s" %3$s>
+                                        <label class="custom-file-label" for="fileUpload">{{ __('%2$s') }}</label>
+                                    </div>
                                 </div>
                             </div>
