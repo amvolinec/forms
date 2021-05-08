c6f805903e56465ffb4e422b9c75f056ef19e8fb
aleksandr.volynec <aleksandr.volynec@addendum.lt>
2021-05-08 15:49:36 +0300

    View composer Groups added

--- src/parts/form-group-checkbox.blade.php
+++ src/parts/form-group-checkbox.blade.php
@@ -2,7 +2,7 @@
                             <div class="col-md-4"></div>
                             <div class="col-md-6">
                                 <div class="form-check">
-                                    <input id="%1$sCheck" type="checkbox" class="form-check-input" name="%1$s"%3$s @if($%4$s->%1$s && $%4$s->%1$s === 1) checked @endif>
+                                    <input id="%1$sCheck" type="checkbox" class="form-check-input" name="%1$s"%3$s @if(isset($%4$s) && $%4$s->%1$s && $%4$s->%1$s === 1) checked @endif>
                                     <label class="form-check-label" for="%1$sCheck">{{ __('%2$s') }}</label>
                                 </div>
                             </div>
