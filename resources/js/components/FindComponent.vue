d22b6ce876352e5fb845e6e1f22f6b4672373cda
Alex <avolinec@gmail.com>
2021-05-09 19:28:36 +0300

    Find component updated

--- resources/js/components/FindComponent.vue
+++ resources/js/components/FindComponent.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="d-inline-block mt-1 ml-2">
-        <div class="d-inline-block drop-find">
-            <input class="d-inline-block form-control form-control-sm" id="find" type="text" v-model="string" @keyup="findString">
+    <div class="d-flex mt-1 ml-2">
+
+        <div class="p-2 input-group">
+            <input class="d-inline-block form-control form-control-sm" id="find" type="text" v-model="string"
+                   @keyup="findString">
             <div class="dropdown-select" v-if="showDrop">
-                <ul v-for="item in items">
-                    <li v-bind:data-id="item.id" @click="setItem(item)">
+                <ul>
+                    <li v-for="item in items" v-bind:data-id="item.id" @click="setItem(item)">
                         <div class="drop-line">
                             <div v-for="column in columns" class="find" v-bind:class="column">{{ item[column] }}</div>
                         </div>
                     </li>
                 </ul>
             </div>
+
+            <div class="input-group-append">
+                <button class="btn btn-sm btn-outline-info" @click="getView"><i class="fa fa-search"></i></button>
+                <button class="btn btn-sm btn-outline-secondary" @click="clearSearch"><i class="fa fa-undo"></i></button>
+            </div>
         </div>
-        <div class="d-inline-block">
-            <div class="drop-btn" @click="getView"><i class="fa fa-search"></i></div>
-        </div>
-        <div class="d-inline-block">
-            <div class="drop-btn" @click="clearSearch"><i class="fa fa-undo"></i></div>
-        </div>
+
     </div>
 </template>
 
@@ -50,6 +52,11 @@ export default {
                 this.getView();
                 return false;
             }
+            if (e.key === 'Escape') {
+                this.showDrop = false
+                return false;
+            }
+            console.log(e.key);
             axios.post(route, {'string': this.string, 'columns': this.fields})
                 .then((r) => {
                     this.showDrop = true;
@@ -58,7 +65,7 @@ export default {
         }, setItem(item) {
             document.location.href = '/' + this.route + '/' + item.id;
             return false;
-        }, getView(){
+        }, getView() {
             document.location.href = '/' + this.route + '/find/' + this.string;
         }, clearSearch() {
             document.location.href = '/' + this.route;
